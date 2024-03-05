bed8ca3ba21866e66376afbb9c9c3b6ace0bcab0
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-05 15:19:13 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/합과 차의 나눗셈/divide-of-sum-and-sub.c
+++ 240305/합과 차의 나눗셈/divide-of-sum-and-sub.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a,b,temp;
-    double c;
-    scanf("%d %d",&a,&b);
-    temp=a,a=a+b,b=temp-b;
-    c=a/b;
+    //int a,b;
+    int temp;
+    double a,b,c;
+    scanf("%lf %lf",&a,&b);
+    temp=a,a+=b,temp-=b;
+    c=a/temp;
     printf("%.2lf",c);// 여기에 코드를 작성해주세요.
     return 0;
 }
