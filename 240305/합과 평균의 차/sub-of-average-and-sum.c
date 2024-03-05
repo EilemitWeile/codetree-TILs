da7d8a2df24c61fed31bc6cc029edced59f47856
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-05 15:35:19 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d,e;
+    scanf("%d %d %d",&a,&b,&c);
+    d=a+b+c;
+    e=d/3;
+    printf("%d\n%d\n%d",d,e,d-e);// 여기에 코드를 작성해주세요.
+    return 0;
+}
