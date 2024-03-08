b8a0875bf8f4c9a08e9f26c70adf41fc936a4788
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-08 09:28:51 +0900

    Title: 합과 평균의 차, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240308/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
+       int d;
+       d=a+b+c;
+       System.out.printf("%d\n%d\n%d\n",d,d/3,d-(d/3)); // 여기에 코드를 작성해주세요.
+    }
+}
