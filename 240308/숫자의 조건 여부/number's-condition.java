6c77b99582c1ae97f103bdf756023654437a9e94
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-08 13:29:55 +0900

    Title: 숫자의 조건 여부, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240308/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       if(a>=113){
+        System.out.printf("1\n");
+       } 
+       else{
+        System.out.printf("0\n");// 여기에 코드를 작성해주세요.
+    }
+}
+}
