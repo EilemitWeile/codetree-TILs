b1af0af4a6ab787ff2a53e14b1feb7a84f081c23
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-09 14:56:52 +0900

    Title: 삼항연산자 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       if(a==1){
+        System.out.printf("t");
+       }
+       else{
+        System.out.printf("f");
+       } // 여기에 코드를 작성해주세요.
+    }
+}
