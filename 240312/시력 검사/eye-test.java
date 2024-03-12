9d7b43359f88d318055eef8a55e16922a2000651
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-12 13:02:15 +0900

    Title: 시력 검사, Time: 170ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240312/시력 검사/eye-test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      double r=sc.nextDouble();
+      double l=sc.nextDouble();
+      if(r>=1.0 && l>=1.0){
+        System.out.printf("High\n");
+      } 
+      else if(r>=0.5 && l>=0.5){
+        System.out.printf("Middle\n");
+      } 
+      else{
+        System.out.printf("Low\n");
+      }  // 여기에 코드를 작성해주세요.
+    }
+}
