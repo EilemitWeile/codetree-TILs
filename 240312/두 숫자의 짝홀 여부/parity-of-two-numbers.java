d8db2fb4dfb34b71278013267e042e0ced7c8d9c
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-12 09:30:11 +0900

    Title: 두 숫자의 짝홀 여부, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/두 숫자의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a%2==0){
+            System.out.printf("even\n");// 여기에 코드를 작성해주세요.
+    }
+    else{
+        System.out.printf("odd\n");
+    }
+    if(b%2==0){
+            System.out.printf("even\n");// 여기에 코드를 작성해주세요.
+    }
+    else{
+        System.out.printf("odd\n");
+    }
+}
+}
