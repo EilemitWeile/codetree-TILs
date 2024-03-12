69e02277818d352b0e0b9f326776568a7eb06918
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-12 10:58:45 +0900

    Title: 비교 연산, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/비교 연산/comparison-operator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       if(a>=b){
+         System.out.printf("1\n");
+       }
+       else{
+         System.out.printf("0\n");
+       }
+       if(a>b){
+         System.out.printf("1\n");
+       }
+       else{
+         System.out.printf("0\n");
+       }if(b>=a){
+         System.out.printf("1\n");
+       }
+       else{
+       System.out.printf("0\n");
+       }
+       if(b>a){
+         System.out.printf("1\n");
+       }
+       else{
+         System.out.printf("0\n");
+       }
+       if(a==b){
+         System.out.printf("1\n");
+       }
+       else{
+         System.out.printf("0\n");
+       }
+       if(a!=b){
+        System.out.printf("1\n");
+       }
+       else{
+         System.out.printf("0\n");
+       } // 여기에 코드를 작성해주세요.
+    }
+}
