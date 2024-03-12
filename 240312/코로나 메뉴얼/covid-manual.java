ea58fb7377d0eda4b54865d95863aaa89662d69a
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-12 17:59:01 +0900

    Title: 코로나 메뉴얼, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240312/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       char c1=sc.next().charAt(0);
+       int f1=sc.nextInt();
+       char c2=sc.next().charAt(0);
+       int f2=sc.nextInt();
+       char c3=sc.next().charAt(0);
+       int f3=sc.nextInt();
+       if(c1=='Y' && f1>=37){
+        if((c2=='Y' && f2>=37) || (c3=='Y' && f3>=37)){
+            System.out.printf("E");
+        }
+        else{
+            System.out.printf("N");
+        }
+       }
+       if(c1=='N' && f1<=37){
+        if((c2=='Y' && f2>=37) && (c3=='Y' && f3>=37)){
+            System.out.printf("E");
+        }
+        else{
+            System.out.printf("N");
+        } // 여기에 코드를 작성해주세요.
+    }
+}
+}
