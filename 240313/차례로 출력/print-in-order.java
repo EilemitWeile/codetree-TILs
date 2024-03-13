c31a85acd8260600c41bc772adcd80cc2b799716
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-13 13:30:23 +0900

    Title: 차례로 출력, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        for(int i=5; i<=17;i++){
+            System.out.printf("%d ",i);// 여기에 코드를 작성해주세요.
+    }
+}
+}
