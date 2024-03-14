c415a70d32b5ea94ddbd3cdcd0fd85570afdd410
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-14 22:26:17 +0900

    Title: 증가시키며 출력하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240314/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       for(int i=5;i<=17;i+=2){
+        System.out.printf("%d ",i);
+       } // 여기에 코드를 작성해주세요.
+    }
+}
