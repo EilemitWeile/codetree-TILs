98397b9724c6de19648bb028146e48a0bbf17d90
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-18 18:32:49 +0900

    Title: 감소시키며 출력하기 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=26;
+       int i=a;
+       while(i>=10){
+        System.out.printf("%d ",i);
+        i--;
+       } // 여기에 코드를 작성해주세요.
+    }
+}
