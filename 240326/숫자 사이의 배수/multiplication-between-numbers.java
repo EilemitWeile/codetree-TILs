a85263076c627734824fcd161651a0bebab3cf57
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-03-26 19:46:02 +0900

    Title: 숫자 사이의 배수, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240326/숫자 사이의 배수/multiplication-between-numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt(); 
+       int cnt=0, sumVal=0;
+       for(int i=a; i<=b; i++){
+        if(i%5==0 || i%7==0){
+            sumVal+=i;
+            cnt++;
+        }
+       }
+       System.out.printf("%d %.1f",sumVal, (double)sumVal/cnt);// 여기에 코드를 작성해주세요.
+    }
+}
