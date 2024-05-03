7d99b701bce65e027fae36550ba715df8342d1b0
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-05-03 09:23:43 +0900

    Title: 0 나올때까지 출력, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n;
+      while(true){
+         n=sc.nextInt();
+         if(n==0){
+            break;
+         }
+         System.out.printf("%d\n",n);
+      } 
+        // 여기에 코드를 작성해주세요.
+    }
+}
