ce900805189cc8ea6d5c7562c2569c67c3322fe1
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-07-10 17:54:01 +0900

    Title: 몫 출력하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240710/몫 출력하기/print-share.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int cnt=0;
+       while(true){
+       int n=sc.nextInt();
+       if(n%2==1){
+        cnt++;
+        continue;
+       }
+       else{
+        System.out.printf("%d\n", n/2);
+        cnt++;
+       }
+       if(cnt>=3){
+        break;
+       }
+       // 여기에 코드를 작성해주세요.
+    }
+    }
+}
