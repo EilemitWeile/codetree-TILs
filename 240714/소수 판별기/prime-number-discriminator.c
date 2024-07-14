7e797db8c2902d80c6425d60fae830dce323329f
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-07-14 21:25:29 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240714/소수 판별기/prime-number-discriminator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    bool satisfied = true;//n이 소수이다.
+
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            satisfied = false;//n이 소수가 아니다.
+        }
+    }
+    if(satisfied = true){
+        printf("P\n");
+    }
+    else{
+        printf("C\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
