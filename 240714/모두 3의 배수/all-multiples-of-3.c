7066bdd1c62be6c72f5d655dd0295a07ff627ef3
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-07-14 21:49:17 +0900

    Title: 모두 3의 배수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240714/모두 3의 배수/all-multiples-of-3.c
+++ 240714/모두 3의 배수/all-multiples-of-3.c
@@ -3,19 +3,22 @@
 
 int main() {
     int arr[5];
-    int a=0;
+    //int a=0;
+    bool satisfied = true;//모두 3의 배수
 
     for(int i=0; i<5; i++){
         scanf("%d\n",&arr[i]);
+        if(arr[i]%3!=0){
+            satisfied = false;
+        }
     }
 
-    bool satisfied = true;//모두 3의 배수
-
+    /*
     for(int i=arr[a]; a<5; a++){
         if(i%3!=0){
             satisfied = false;//모두 3의 배수 아님
         }
-    }
+    }*/
     if(satisfied == true){
         printf("1\n");
     }
