b89508a2ede4a4f2286696a56998ddeb7258b3e0
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-07-14 21:13:58 +0900

    Title: ab 사이에 없는 c, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240714/ab 사이에 없는 c/c-between-a-and-b-2.c
+++ 240714/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -5,18 +5,18 @@ int main() {
     int a,b,c;
     scanf("%d %d %d", &a,&b,&c);
 
-    bool satisfied = true;
+    bool satisfied = true;//배수있음
 
     for(int i=a; i<=b; i++){
-        if(c%2!=0){
+        if(c%i!=0){//배수 없음
             satisfied = false;
         }
     }
     if(satisfied == true){
-        printf("YES\n");
+        printf("NO\n");
     }
     else{
-        printf("NO\n");
+        printf("YES\n");
     }
     // 여기에 코드를 작성해주세요.
     return 0;
