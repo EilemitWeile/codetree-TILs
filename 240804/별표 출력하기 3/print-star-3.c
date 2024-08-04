5bdcd98fd7d29d2cb19e507299717f06763c1c03
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-08-04 21:26:08 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/별표 출력하기 3/print-star-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            printf("  ");
+        }
+        for(int k=0; k<(2*n)-(2*i)-1; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
