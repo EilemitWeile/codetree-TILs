a4b5358a7f5a43080f45ce1963ffa679637dd9b6
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-08-26 21:43:01 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240826/1-9 정사각형/1-9-square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int cnt=1;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%d",cnt);
+            cnt++;
+            if(cnt>9){
+                cnt=1;
+            }
+        }
+        printf("\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
