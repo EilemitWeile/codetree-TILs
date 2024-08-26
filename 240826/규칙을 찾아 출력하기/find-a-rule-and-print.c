ccdee82972960692832210633a16918e5fe81b7f
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-08-26 20:52:43 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240826/규칙을 찾아 출력하기/find-a-rule-and-print.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==n-1){
+                printf("* ");
+            }
+            else{
+                if(i>j){
+                    printf("* ");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+        }
+        printf("\n");
+    }        
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
