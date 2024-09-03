c753faecede41d442b02c4b3b4fc5aa2b307dca0
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-03 21:47:20 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/소수 구하기/get-prime.c
+++ 240903/소수 구하기/get-prime.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int n;
     scanf("%d",&n);
 
     for(int i=1; i<=n; i++){
-        int divisor_cnt=0;
-        for(int j=1; j<=i; j++){
-            if(i%j==0){
-                divisor_cnt++;
-            }
-            if(divisor_cnt==2){
-                printf("%d ",j);
-        }
+        bool primeN=true;
+
+        if(i==1){
+            continue;
         }
         
+        for(int j=2; j<i; j++){
+            if(i%j==0){
+                primeN=false;
+            }
+            
+        }
+        if(primeN){
+            printf("%d ",i);
+        }
+       
     }
     // 여기에 코드를 작성해주세요.
     return 0;
