c84cc2372f191fd3d119f1c074dc8f0231c70155
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-04 09:13:58 +0900

    Title: 배열 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240904/배열 채우기/filling-array.c
+++ 240904/배열 채우기/filling-array.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int answer=0;
+    int condition=0;
     int arr[10];
     for(int i=0; i<10; i++){
         scanf("%d",&arr[i]);
         if(arr[i]==0){
-            answer=i;
-            for(int j=i-1; j>=0; j--){
-                printf("%d ",arr[j]);
-            }
+            break;
         }
+        condition++;
+    }
+    for(int j=condition-1; j>=0; j--){
+        printf("%d ",arr[j]);
     }
     
     // 여기에 코드를 작성해주세요.
