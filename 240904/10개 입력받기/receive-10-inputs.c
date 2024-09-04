30cae345ab7a92829126e7fd145cabfe893922bd
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-04 09:22:01 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240904/10개 입력받기/receive-10-inputs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int sum=0;
+    double avg=0;
+    int condition=0;
+    for(int i=0; i<10; i++){
+        scanf("%d",&arr[i]);
+        condition++;
+        sum+=arr[i];
+        if(arr[i]==0){
+            break;
+        }
+        avg=(double)sum/condition;
+
+    }
+    printf("%d %.1f",sum,avg);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
