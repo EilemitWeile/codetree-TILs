99aeda10a40fe5967e6a984f0f185aa8499589a3
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-06 18:43:23 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240906/숫자들의 배수/multiple-of-numbers.c
+++ 240906/숫자들의 배수/multiple-of-numbers.c
@@ -3,18 +3,19 @@
 int main() {
     int arr[10];
     scanf("%d",&arr[0]);
+    int cnt=0;
     for(int i=1; i<10; i++){
         arr[i]=(i+1)*arr[0];
     }
     for(int i=0; i<10; i++){
-        int cnt=0;
+        printf("%d ",arr[i]);
         if(arr[i]%5==0){
             cnt++;
             if(cnt==2){
                 break;
             }
         }
-        printf("%d ",arr[i]);
+        
     }
     // 여기에 코드를 작성해주세요.
     return 0;
