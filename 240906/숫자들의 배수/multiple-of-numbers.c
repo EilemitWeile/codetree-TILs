c707839be6beac80b0ad0c456257bc97c4615653
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-06 18:41:47 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240906/숫자들의 배수/multiple-of-numbers.c
+++ 240906/숫자들의 배수/multiple-of-numbers.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d",&n);
-    int arr[10]={n};
+    int arr[10];
+    scanf("%d",&arr[0]);
     for(int i=1; i<10; i++){
-        arr[i]=(i+1)*n;
+        arr[i]=(i+1)*arr[0];
     }
     for(int i=0; i<10; i++){
         int cnt=0;
