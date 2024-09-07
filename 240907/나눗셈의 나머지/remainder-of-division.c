fdef0e0db563e5b0b9e2163ca0b8a38a65c707b9
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-07 20:48:25 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240907/나눗셈의 나머지/remainder-of-division.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,answer_sum=0;
+    scanf("%d %d",&a,&b);
+    int remainder_cnt[b];
+
+    for(int i=0; i<b; i++){
+        remainder_cnt[i]=0;
+    }
+
+    while(1){
+        int remainder=a%b;
+        a=a/b;
+        for(int i=0; i<b; i++){
+            if(i==remainder){
+                remainder_cnt[i]++;
+            }
+        }
+        if(a<=1){
+            break;
+        }
+    }
+    for(int i=0; i<b; i++){
+        answer_sum+=remainder_cnt[i]*remainder_cnt[i];
+    }
+    
+    printf("%d",answer_sum);
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
