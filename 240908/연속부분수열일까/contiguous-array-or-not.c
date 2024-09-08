7c2f57d14c73af5f73f4297c4f60e3d0da988904
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-08 20:48:27 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/연속부분수열일까/contiguous-array-or-not.c
+++ 240908/연속부분수열일까/contiguous-array-or-not.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int n1,n2;
     scanf("%d %d",&n1,&n2);
     int A[100]={0,};
     int B[100]={0,};
-    int cnt=0;
 
     for(int i=0; i<n1; i++){
         scanf("%d",&A[i]);
@@ -13,13 +13,25 @@ int main() {
      for(int i=0; i<n2; i++){
         scanf("%d",&B[i]);
     }
-    for(int i=0; i<n2; i++){
-        if(A[i]==B[i]){
-            cnt++;
-       }
+
+    for(int i=0; i<n1; i++){
+        bool success=true;
+        for(int j=0; j<n2; j++){
+            if(i+j>=n1){
+                success=false;
+                break;
+            }
+            if(A[i+j]!=B[j]){
+                success=false;
+                break;
+            }
+        }
+        if(success==true){
+            printf("Yes");
+            return 0;
+        }
     }
-    if(cnt>0) printf("Yes");
-    else printf("No");
+    printf("No");
     // 여기에 코드를 작성해주세요.
     return 0;
 }
