54804333f86373f9efafeba53af178bac0d31a3a
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-10 00:24:50 +0900

    Title: 999 또는 -999, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240910/999 또는 -999/999-or-999.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int min_val=0;
+    int max_val=0;
+    int arr[100]={0,};
+    min_val=arr[0];
+    max_val=arr[0];
+    for(int i=0; i<100; i++){
+        scanf("%d ",&arr[i]);
+        
+
+        if(arr[i]==-999 || arr[i]==999){
+            break;
+        }
+        if(arr[i]>max_val){
+            max_val=arr[i];
+        }
+        if(arr[i]<min_val){
+            min_val=arr[i];
+        }
+        
+    }
+    printf("%d %d",max_val,min_val);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
