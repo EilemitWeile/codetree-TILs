276cbe2c889f395ac6dfac2a42c4ffc065f053d2
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-10 22:37:32 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/n개의 숫자 중 최대 2개/two-max-of-n-num.c
+++ 240910/n개의 숫자 중 최대 2개/two-max-of-n-num.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
+
+void bubbleSort(int arr[], int N){
+    int i,j,temp;
+    for(i=N-1; i>0; i--){
+        for(int j=0; j<i; j++){
+            if(arr[j]<arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+}
+
 int main() {
-    int N,arr[100]={0,},max1,max2;
+    int N,arr[100]={0,};
     scanf("%d",&N);
     for(int i=0; i<N; i++){
         scanf("%d ",&arr[i]);
     }
-    if(arr[0]>arr[1]){
-        max1=arr[0];
-        max2=arr[1];
-    }
-    else{
-        max1=arr[1];
-        max2=arr[0];
-    }
-    for(int i=0; i<N; i++){
-        if(arr[i]>=max1){
-            max2=max1;
-            max1=arr[i];
-        }
-        else if(arr[i]>max2){
-            max2=arr[i];
-        }
-    }
-    printf("%d %d",max1,max2);
+    bubbleSort(arr,N);
+
+    printf("%d %d",arr[0],arr[1]);
 
     // 여기에 코드를 작성해주세요.
     return 0;
