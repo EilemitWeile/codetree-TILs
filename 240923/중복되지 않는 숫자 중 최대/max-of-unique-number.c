f0ba49b47eb60b1a3ee3b433796749a4cc0a6bb3
김민재 Francis <117156033+EilemitWeile@users.noreply.github.com>
2024-09-23 16:29:01 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/중복되지 않는 숫자 중 최대/max-of-unique-number.c
+++ 240923/중복되지 않는 숫자 중 최대/max-of-unique-number.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 //#include <stdbool.h>
 
-int main() {
+int main() 
+{
     int N;
     scanf("%d",&N);
     int arr[1000]={0,};
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N; i++)
+    {
         scanf("%d",&arr[i]);
     }
     int MAX=-1;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(arr[i]==arr[j]){
-                break;
-            }
-            else if(arr[i]>arr[j]){
-                if(MAX==arr[i]){
-                    MAX=-1;
+    for(int i=0; i<N; i++)
+    {
+        int curr_num=arr[i];
+        if(curr_num>MAX)
+        {
+            int count=0;
+            for(int j=0; j<N; j++)
+            {
+                if(arr[j]==curr_num)
+                {
+                    count++;
                 }
-                else if(arr[i]>MAX){
-                    MAX=arr[i];
-                }
             }
-            
+            if(count==1)
+            {
+                MAX=curr_num;
+            }
         }
     }
     printf("%d",MAX);
